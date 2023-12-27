a9bdd10415d41528c83fbcd671c7d45e0134a69f
Dmytro Cheva <dmytro.cheva@gmail.com>
2023-12-27 11:56:30 +0200

    Change .env

---
+++ config/notifications.alerts.php
@@ -0,0 +1,345 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015 to present Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+return [
+    // Core Notifications
+    'created_user' => [
+        'label' => 'notifications::alerts.created_user',
+        'handlers' => [
+            'mail' => \Seat\Notifications\Notifications\Seat\Mail\CreatedUser::class,
+            'discord' => \Seat\Notifications\Notifications\Seat\Discord\CreatedUser::class,
+            'slack' => \Seat\Notifications\Notifications\Seat\Slack\CreatedUser::class,
+        ],
+    ],
+    'disabled_token' => [
+        'label' => 'notifications::alerts.disabled_token',
+        'handlers' => [
+            'discord' => \Seat\Notifications\Notifications\Seat\Discord\DisabledToken::class,
+            'slack' => \Seat\Notifications\Notifications\Seat\Slack\DisabledToken::class,
+        ],
+    ],
+    'enabled_token' => [
+        'label' => 'notifications::alerts.enabled_token',
+        'handlers' => [
+            'discord' => \Seat\Notifications\Notifications\Seat\Discord\EnabledToken::class,
+            'slack' => \Seat\Notifications\Notifications\Seat\Slack\EnabledToken::class,
+        ],
+    ],
+    //
+    // Squads
+    //
+    'squad_application' => [
+        'label' => 'notifications::alerts.squad_application',
+        'handlers' => [
+            'mail' => \Seat\Notifications\Notifications\Seat\Mail\SquadApplicationNotification::class,
+            'slack' => \Seat\Notifications\Notifications\Seat\Slack\SquadApplicationNotification::class,
+            'discord' => \Seat\Notifications\Notifications\Seat\Discord\SquadApplicationNotification::class,
+        ],
+    ],
+    'squad_member' => [
+        'label' => 'notifications::alerts.squad_member',
+        'handlers' => [
+            'mail' => \Seat\Notifications\Notifications\Seat\Mail\SquadMemberNotification::class,
+            'slack' => \Seat\Notifications\Notifications\Seat\Slack\SquadMemberNotification::class,
+            'discord' => \Seat\Notifications\Notifications\Seat\Discord\SquadMemberNotification::class,
+        ],
+    ],
+    'squad_member_removed' => [
+        'label' => 'notifications::alerts.squad_member_removed',
+        'handlers' => [
+            'mail' => \Seat\Notifications\Notifications\Seat\Mail\SquadMemberRemovedNotification::class,
+            'slack' => \Seat\Notifications\Notifications\Seat\Slack\SquadMemberRemovedNotification::class,
+            'discord' => \Seat\Notifications\Notifications\Seat\Discord\SquadMemberRemovedNotification::class,
+        ],
+    ],
+    //
+    // Killmails
+    //
+    'Killmail' => [
+        'label' => 'notifications::alerts.killmails',
+        'handlers' => [
+            'mail' => \Seat\Notifications\Notifications\Characters\Mail\Killmail::class,
+            'slack' => \Seat\Notifications\Notifications\Characters\Slack\Killmail::class,
+            'discord' => \Seat\Notifications\Notifications\Characters\Discord\Killmail::class,
+        ],
+    ],
+    //
+    // Esi Character Notifications
+    //
+    'AllAnchoringMsg' => [
+        'label'   => 'notifications::alerts.alliance_anchoring',
+        'handlers' => [
+            'slack' => \Seat\Notifications\Notifications\Structures\Slack\AllAnchoringMsg::class,
+            'discord' => \Seat\Notifications\Notifications\Structures\Discord\AllAnchoringMsg::class,
+        ],
+    ],
+    'AllWarDeclaredMsg' => [
+        'label'   => 'notifications::alerts.alliance_war_declared',
+        'handlers' => [
+            'discord' => \Seat\Notifications\Notifications\Wars\Discord\AllWarDeclaredMsg::class,
+            'slack' => \Seat\Notifications\Notifications\Wars\Slack\AllWarDeclaredMsg::class,
+        ],
+    ],
+    'AllWarInvalidatedMsg' => [
+        'label'   => 'notifications::alerts.alliance_war_invalidated',
+        'handlers' => [
+            'discord' => \Seat\Notifications\Notifications\Wars\Discord\AllWarInvalidatedMsg::class,
+            'slack' => \Seat\Notifications\Notifications\Wars\Slack\AllWarInvalidatedMsg::class,
+        ],
+    ],
+    'AllianceCapitalChanged' => [
+        'label'   => 'notifications::alerts.alliance_capital_changed',
+        'handlers' => [
+            'discord' => \Seat\Notifications\Notifications\Alliances\Discord\AllianceCapitalChanged::class,
+            'slack' => \Seat\Notifications\Notifications\Alliances\Slack\AllianceCapitalChanged::class,
+        ],
+    ],
+    'AllyJoinedWarAggressorMsg' => [
+        'label'   => 'notifications::alerts.ally_joined_war_aggressor',
+        'handlers' => [
+            'discord' => \Seat\Notifications\Notifications\Wars\Discord\AllyJoinedWarAggressorMsg::class,
+            'slack' => \Seat\Notifications\Notifications\Wars\Slack\AllyJoinedWarAggressorMsg::class,
+        ],
+    ],
+    'AllyJoinedWarAllyMsg' => [
+        'label'   => 'notifications::alerts.ally_joined_war_ally',
+        'handlers' => [
+            'discord' => \Seat\Notifications\Notifications\Wars\Discord\AllyJoinedWarAllyMsg::class,
+            'slack' => \Seat\Notifications\Notifications\Wars\Slack\AllyJoinedWarAllyMsg::class,
+        ],
+    ],
+    'AllyJoinedWarDefenderMsg' => [
+        'label'   => 'notifications::alerts.ally_joined_war_defender',
+        'handlers' => [
+            'discord' => \Seat\Notifications\Notifications\Wars\Discord\AllyJoinedWarDefenderMsg::class,
+            'slack' => \Seat\Notifications\Notifications\Wars\Slack\AllyJoinedWarDefenderMsg::class,
+        ],
+    ],
+    'BillPaidCorpAllMsg' => [
+        'label'   => 'notifications::alerts.bill_paid_corporation_alliance',
+        'handlers' => [
+            'discord' => \Seat\Notifications\Notifications\Corporations\Discord\BillPaidCorpAllMsg::class,
+            'slack' => \Seat\Notifications\Notifications\Corporations\Slack\BillPaidCorpAllMsg::class,
+        ],
+    ],
+    'CharLeftCorpMsg' => [
+        'label'   => 'notifications::alerts.character_left_corporation',
+        'handlers' => [
+            'discord' => \Seat\Notifications\Notifications\Corporations\Discord\CharLeftCorpMsg::class,
+            'mail' => \Seat\Notifications\Notifications\Corporations\Mail\CharLeftCorpMsg::class,
+            'slack' => \Seat\Notifications\Notifications\Corporations\Slack\CharLeftCorpMsg::class,
+        ],
+    ],
+    'CorpAllBillMsg' => [
+        'label'   => 'notifications::alerts.corporation_alliance_bill',
+        'handlers' => [
+            'discord' => \Seat\Notifications\Notifications\Corporations\Discord\CorpAllBillMsg::class,
+            'mail' => \Seat\Notifications\Notifications\Corporations\Mail\CorpAllBillMsg::class,
+            'slack' => \Seat\Notifications\Notifications\Corporations\Slack\CorpAllBillMsg::class,
+        ],
+    ],
+    'CorpAppNewMsg' => [
+        'label'   => 'notifications::alerts.corporation_application_new',
+        'handlers' => [
+            'discord' => \Seat\Notifications\Notifications\Corporations\Discord\CorpAppNewMsg::class,
+            'slack' => \Seat\Notifications\Notifications\Corporations\Slack\CorpAppNewMsg::class,
+        ],
+    ],
+    'MoonminingExtractionFinished' => [
+        'label'   => 'notifications::alerts.moon_mining_extraction_finished',
+        'handlers' => [
+            'mail' => \Seat\Notifications\Notifications\Structures\Mail\MoonMiningExtractionFinished::class,
+            'slack' => \Seat\Notifications\Notifications\Structures\Slack\MoonMiningExtractionFinished::class,
+            'discord' => \Seat\Notifications\Notifications\Structures\Discord\MoonMiningExtractionFinished::class,
+        ],
+    ],
+    'MoonminingExtractionStarted' => [
+        'label'   => 'notifications::alerts.moon_mining_extraction_started',
+        'handlers' => [
+            'mail' => \Seat\Notifications\Notifications\Structures\Mail\MoonMiningExtractionStarted::class,
+            'slack' => \Seat\Notifications\Notifications\Structures\Slack\MoonMiningExtractionStarted::class,
+            'discord' => \Seat\Notifications\Notifications\Structures\Discord\MoonMiningExtractionStarted::class,
+        ],
+    ],
+    'OrbitalAttacked' => [
+        'label'   => 'notifications::alerts.orbital_attacked',
+        'handlers' => [
+            'slack' => \Seat\Notifications\Notifications\Structures\Slack\OrbitalAttacked::class,
+            'discord' => \Seat\Notifications\Notifications\Structures\Discord\OrbitalAttacked::class,
+        ],
+    ],
+    'OwnershipTransferred' => [
+        'label'   => 'notifications::alerts.ownership_transferred',
+        'handlers' => [
+            'slack' => \Seat\Notifications\Notifications\Structures\Slack\OwnershipTransferred::class,
+            'discord' => \Seat\Notifications\Notifications\Structures\Discord\OwnershipTransferred::class,
+        ],
+    ],
+    'RaffleCreated' => [
+        'label'   => 'notifications::alerts.raffle_created',
+        'handlers' => [
+            'slack' => \Seat\Notifications\Notifications\Characters\Slack\RaffleCreated::class,
+            'discord' => \Seat\Notifications\Notifications\Characters\Discord\RaffleCreated::class,
+        ],
+    ],
+    'RaffleExpired' => [
+        'label'   => 'notifications::alerts.raffle_expired',
+        'handlers' => [
+            'slack' => \Seat\Notifications\Notifications\Characters\Slack\RaffleExpired::class,
+            'discord' => \Seat\Notifications\Notifications\Characters\Discord\RaffleExpired::class,
+        ],
+    ],
+    'RaffleFinished' => [
+        'label'   => 'notifications::alerts.raffle_finished',
+        'handlers' => [
+            'slack' => \Seat\Notifications\Notifications\Characters\Slack\RaffleFinished::class,
+            'discord' => \Seat\Notifications\Notifications\Characters\Discord\RaffleFinished::class,
+        ],
+    ],
+    'ResearchMissionAvailableMsg' => [
+        'label'   => 'notifications::alerts.research_mission_available',
+        'handlers' => [
+            'slack' => \Seat\Notifications\Notifications\Characters\Slack\ResearchMissionAvailableMsg::class,
+            'discord' => \Seat\Notifications\Notifications\Characters\Discord\ResearchMissionAvailableMsg::class,
+        ],
+    ],
+    'SovStructureDestroyed' => [
+        'label'   => 'notifications::alerts.sovereignty_structure_destroyed',
+        'handlers' => [
+            'discord' => \Seat\Notifications\Notifications\Sovereignties\Discord\SovStructureDestroyed::class,
+            'mail' => \Seat\Notifications\Notifications\Sovereignties\Mail\SovStructureDestroyed::class,
+            'slack' => \Seat\Notifications\Notifications\Sovereignties\Slack\SovStructureDestroyed::class,
+        ],
+    ],
+    'SovStructureReinforced' => [
+        'label'   => 'notifications::alerts.sovereignty_structure_reinforced',
+        'handlers' => [
+            'discord' => \Seat\Notifications\Notifications\Sovereignties\Discord\SovStructureReinforced::class,
+            'mail' => \Seat\Notifications\Notifications\Sovereignties\Mail\SovStructureReinforced::class,
+            'slack' => \Seat\Notifications\Notifications\Sovereignties\Slack\SovStructureReinforced::class,
+        ],
+    ],
+    'StoryLineMissionAvailableMsg' => [
+        'label'   => 'notifications::alerts.story_line_mission_available',
+        'handlers' => [
+            'slack' => \Seat\Notifications\Notifications\Characters\Slack\StoryLineMissionAvailableMsg::class,
+            'discord' => \Seat\Notifications\Notifications\Characters\Discord\StoryLineMissionAvailableMsg::class,
+        ],
+    ],
+    'StructureAnchoring' => [
+        'label'   => 'notifications::alerts.structure_anchoring',
+        'handlers' => [
+            'slack' => \Seat\Notifications\Notifications\Structures\Slack\StructureAnchoring::class,
+            'discord' => \Seat\Notifications\Notifications\Structures\Discord\StructureAnchoring::class,
+        ],
+    ],
+    'StructureDestroyed' => [
+        'label'   => 'notifications::alerts.structure_destroyed',
+        'handlers' => [
+            'slack' => \Seat\Notifications\Notifications\Structures\Slack\StructureDestroyed::class,
+            'discord' => \Seat\Notifications\Notifications\Structures\Discord\StructureDestroyed::class,
+        ],
+    ],
+    'StructureFuelAlert' => [
+        'label'   => 'notifications::alerts.structure_fuel_alert',
+        'handlers' => [
+            'mail' => \Seat\Notifications\Notifications\Structures\Mail\StructureFuelAlert::class,
+            'slack' => \Seat\Notifications\Notifications\Structures\Slack\StructureFuelAlert::class,
+            'discord' => \Seat\Notifications\Notifications\Structures\Discord\StructureFuelAlert::class,
+        ],
+    ],
+    'StructureLostArmor' => [
+        'label'   => 'notifications::alerts.structure_lost_armor',
+        'handlers' => [
+            'slack' => \Seat\Notifications\Notifications\Structures\Slack\StructureLostArmor::class,
+            'discord' => \Seat\Notifications\Notifications\Structures\Discord\StructureLostArmor::class,
+        ],
+    ],
+    'StructureLostShields' => [
+        'label'   => 'notifications::alerts.structure_lost_shield',
+        'handlers' => [
+            'slack' => \Seat\Notifications\Notifications\Structures\Slack\StructureLostShields::class,
+            'discord' => \Seat\Notifications\Notifications\Structures\Discord\StructureLostShields::class,
+        ],
+    ],
+    'StructureServicesOffline' => [
+        'label'   => 'notifications::alerts.structure_services_offline',
+        'handlers' => [
+            'mail' => \Seat\Notifications\Notifications\Structures\Mail\StructureServicesOffline::class,
+            'slack' => \Seat\Notifications\Notifications\Structures\Slack\StructureServicesOffline::class,
+            'discord' => \Seat\Notifications\Notifications\Structures\Discord\StructureServicesOffline::class,
+        ],
+    ],
+    'StructureUnanchoring' => [
+        'label'   => 'notifications::alerts.structure_unanchoring',
+        'handlers' => [
+            'slack' => \Seat\Notifications\Notifications\Structures\Slack\StructureUnanchoring::class,
+            'discord' => \Seat\Notifications\Notifications\Structures\Discord\StructureUnanchoring::class,
+        ],
+    ],
+    'StructureUnderAttack' => [
+        'label'   => 'notifications::alerts.structure_under_attack',
+        'handlers' => [
+            'mail' => \Seat\Notifications\Notifications\Structures\Mail\StructureUnderAttack::class,
+            'slack' => \Seat\Notifications\Notifications\Structures\Slack\StructureUnderAttack::class,
+            'discord' => \Seat\Notifications\Notifications\Structures\Discord\StructureUnderAttack::class,
+        ],
+    ],
+    'StructureWentHighPower' => [
+        'label'   => 'notifications::alerts.structure_went_high_power',
+        'handlers' => [
+            'slack' => \Seat\Notifications\Notifications\Structures\Slack\StructureWentHighPower::class,
+            'discord' => \Seat\Notifications\Notifications\Structures\Discord\StructureWentHighPower::class,
+        ],
+    ],
+    'StructureWentLowPower' => [
+        'label'   => 'notifications::alerts.structure_went_low_power',
+        'handlers' => [
+            'slack' => \Seat\Notifications\Notifications\Structures\Slack\StructureWentLowPower::class,
+            'discord' => \Seat\Notifications\Notifications\Structures\Discord\StructureWentLowPower::class,
+        ],
+    ],
+    'inactive_member' => [
+        'label' => 'notifications::alerts.war_inactive_member',
+        'handlers' => [
+            'discord' => \Seat\Notifications\Notifications\Corporations\Discord\InActiveCorpMember::class,
+            'slack' => \Seat\Notifications\Notifications\Corporations\Slack\InActiveCorpMember::class,
+        ],
+    ],
+    'contract_created' => [
+        'label' => 'notifications::alerts.contract_created',
+        'handlers' => [
+            'slack' => \Seat\Notifications\Notifications\Contracts\Slack\ContractNotification::class,
+            'discord' => \Seat\Notifications\Notifications\Contracts\Discord\ContractNotification::class,
+        ],
+    ],
+    // even though the test notification can't be added to a notification group, it is here for consistency
+    'test_integration' => [
+        'label' => 'notifications::notifications.test_integration', // Since it's hidden, it shouldn't really matter
+        'handlers' => [
+            'mail' => \Seat\Notifications\Notifications\Seat\Mail\TestNotification::class,
+            'slack' => \Seat\Notifications\Notifications\Seat\Slack\TestNotification::class,
+            'discord'=>\Seat\Notifications\Notifications\Seat\Discord\TestNotification::class,
+        ],
+        'visible' => false,
+    ],
+];
---
+++ public/web/js/seat.js
@@ -0,0 +1,183 @@
+// Add the X-CSRF Token header to ajax requests
+// Ref: http://laravel.com/docs/5.1/routing#csrf-x-csrf-token
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+
+// Generic 'confirm' dialog code for forms.
+// Make your submit button part of class confirmform, and viola
+// Optional: You can describe the action by adding a "data-seat-action" attribute.
+var currentForm;
+$(document).on("click", ".confirmform", function (e) {
+    if ($(this).attr('form') === undefined) {
+        currentForm = $(this).closest("form");
+    } else {
+        currentForm = $('#'.concat($(this).attr('form')))
+    }
+    e.preventDefault();
+    var action = $(this).data('seat-action');
+    var message = 'Are you sure you want to continue?';
+    if (typeof action !== 'undefined') {
+        message = `Are you sure you want to ${action}?`;
+    }
+    bootbox.confirm(message, function (confirmed) {
+        if (confirmed) {
+            currentForm.submit();
+        }
+    });
+});
+
+// Deletion 'confirm' dialog.
+// Make your submit button part of class confirmdelete, and viola.
+// You can add an entity name by adding a "data-seat-entity" attribute.
+var currentForm;
+$(document).on("click", ".confirmdelete", function (e) {
+    if ($(this).attr('form') === undefined) {
+        currentForm = $(this).closest("form");
+    } else {
+        currentForm = $('#'.concat($(this).attr('form')))
+    }
+    e.preventDefault();
+    entity = $(this).data('seat-entity');
+    var message = 'Are you sure you want to delete this?';
+    if (typeof entity !== 'undefined') {
+        message = `Are you sure you want to delete this ${entity}?`;
+    }
+    bootbox.confirm(message, function (confirmed) {
+        if (confirmed) {
+            currentForm.submit();
+        }
+    });
+});
+
+// Generic 'confirm' dialog code for links.
+// Make your link button part of class confirmlink, and viola
+$(document).on("click", "a.confirmlink", function (event) {
+    event.preventDefault();
+    var url = $(this).attr("href");
+    bootbox.confirm("Are you sure you want to continue?", function (confirmed) {
+        if (confirmed) {
+            window.location = url;
+        }
+    });
+});
+
+// Init the jQuery unveil plugin. If the
+// viewport come into 100px, start loading
+// the image
+//
+// http://luis-almeida.github.io/unveil/
+$(document).ready(function () {
+    $("img").unveil(100);
+});
+
+// Enable bootstrap popovers
+$("[data-toggle=popover]").popover();
+
+// Enable bootstrap tooltips
+$("[data-toggle=tooltip]").tooltip();
+
+// Initialize DataTables on <table> tags
+// with the 'datatable' class.
+//
+// https://www.datatables.net/
+$(document).ready(function () {
+    $("table.datatable").DataTable({
+        paging: false,
+        order: []
+    });
+});
+
+// Configure some defaults for Datatables
+$.extend(true, $.fn.dataTable.defaults, {
+    responsive: true,
+    autoWidth: false,
+    order: [[0, 'desc']]
+});
+
+// put some animation on the caret neat to the user dropdown
+$(document).ready(function () {
+    $('#user-dropdown').on('show.bs.dropdown', function () {
+        $('#user-dropdown').find('i.fa-caret-left').addClass('fa-rotate-270');
+    }).on('hide.bs.dropdown', function () {
+        $('#user-dropdown').find('i.fa-caret-left').removeClass('fa-rotate-270');
+    });
+
+    $('button[data-widget="esi-update"]').on('click', function() {
+        var button = $(this);
+        button.find('i').addClass('fa-spin');
+
+        $.ajax({
+            url: button.data('url'),
+            method: 'POST',
+            data: {
+                type: button.data('type'),
+                entity_id: button.data('entity'),
+                job_name: button.data('job')
+            },
+            success: function() {
+                button.find('i').removeClass('fa-spin');
+            },
+            error: function(e) {
+                button.addClass('btn-danger');
+                button.removeClass('btn-light');
+
+                console.error(e);
+            }
+        });
+    });
+});
+
+// Helper function to mimic the PHP human_readable method
+function human_readable(data, type, row) {
+    if (type == 'display') {
+        var date = moment.utc(data, "YYYY-MM-DD hh:mm:ss").fromNow();
+        return '<span data-toggle="tooltip" data-placement="top" title="' + data + '">' + date + "</span>";
+    }
+
+    return data;
+}
+
+// Helper function to abbreviate numbers to their SI suffix
+var SI_SYMBOL = ["", "k", "M", "G", "T", "P", "E"];
+
+function abbreviateNumber(number){
+
+  // what tier? (determines SI symbol)
+  var tier = Math.log10(number) / 3 | 0;
+
+  // if zero, we don't need a suffix
+  if(tier == 0) return number.toFixed(1);
+
+  // get suffix and determine scale
+  var suffix = SI_SYMBOL[tier];
+  var scale = Math.pow(10, tier * 3);
+
+  // scale the number
+  var scaled = number / scale;
+
+  // format number and add suffix
+  return scaled.toFixed(1) + suffix;
+}
+
+// Helper function to wrap long strings in multiple lines separated
+// str:   The string to be wrapped.
+// width: The column width (a number, default: 75)
+// brk:   The character(s) to be inserted at every break. (default: ‘n’)
+// cut:   The cut: a Boolean value (false by default). See PHP docs for more info. (http://us3.php.net/manual/en/function.wordwrap.php)
+
+function wordwrap( str, width, brk, cut ) {
+
+  brk = brk || 'n';
+  width = width || 75;
+  cut = cut || false;
+
+  if (!str) { return str; }
+
+  var regex = '.{1,' +width+ '}(\s|$)' + (cut ? '|.{' +width+ '}|.+$' : '|\S+?(\s|$)');
+
+  return str.match( RegExp(regex, 'g') ).join( brk );
+
+}
